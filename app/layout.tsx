186955c50137ddaf0e6850301373aa02a92b0fcf
Ronish Dhakal <ronishdhakal1@gmail.com>
2026-04-15 10:29:21 +0545

    Analytics Added

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,6 +41,20 @@ export default function RootLayout({
           crossOrigin="anonymous"
           strategy="afterInteractive"
         />
+
+        {/* Google Analytics (gtag.js) */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-3H8YGYXHPP"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-3H8YGYXHPP');
+          `}
+        </Script>
       </head>
 
       <body className="min-h-full flex flex-col">
